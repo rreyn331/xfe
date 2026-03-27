fa640ff09900e76f3dabdc43e352d13e4f0bfc8d
rreyn331 <110791312+rreyn331@users.noreply.github.com>
2026-03-27 14:55:21 +0000

    Add files via upload
    updated to work with cpm script so it will ask for authorization and install packages

--- src/XFilePackage.cpp
+++ src/XFilePackage.cpp
@@ -404,11 +404,11 @@ long XFilePackage::onCmdInstall(FXObject*, FXSelector, void*)
     FXString ext = FXPath::extension(filename);
     if (comparecase(ext, "rpm") == 0)
     {
-        cmd = "rpm -Uvh " + filename;
+        cmd = FXString("/usr/bin/cpm i -y ") + xf_quote(filename);
     }
     else if (comparecase(ext, "deb") == 0)
     {
-        cmd = "dpkg -i " + filename;
+        cmd = FXString("/usr/bin/cpm i -y ") + xf_quote(filename);
     }
     else
     {
@@ -451,14 +451,14 @@ long XFilePackage::onCmdUninstall(FXObject*, FXSelector, void*)
         // Get package name
         package = FXPath::name(filename);
         package = package.section('-', 0);
-        cmd = "rpm -e " + package;
+        cmd = FXString("/usr/bin/cpm r -y ") + package;
     }
     else if (comparecase(ext, "deb") == 0)
     {
         // Get package name
         package = FXPath::name(filename);
         package = package.section('_', 0);
-        cmd = "dpkg -r " + package;
+        cmd = FXString("/usr/bin/cpm r -y ") + package;
     }
     else
     {
